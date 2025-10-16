1c67de7399078d48b5e1841ac2789a68dbded809
kudoshinichi260 <nguyenhoanggiadai1412@gmail.com>
2025-10-16 11:43:56 +0700

    add SplashCursor components

---
+++ src/pages/home/sections/ContactSection.tsx
@@ -0,0 +1,16 @@
+import FuzzyText from "../../../components/Reactbits/FuzzyText/FuzzyText"
+
+function ContactSection() {
+ 
+    return (
+      <section content="">
+          <FuzzyText 
+            baseIntensity={0.2} 
+          >
+            404
+          </FuzzyText>
+      </section>
+    )
+}
+
+export default ContactSection
--- src/pages/home/sections/ProjectSection.tsx
+++ src/pages/home/sections/ProjectSection.tsx
@@ -1,20 +1,9 @@
-import ModelViewer from "../../../components/model3d/ModelViewer";
-import logo3dv3 from "../../../assets/glb/logo3dv3.glb"
-import FuzzyText from "../../../components/Reactbits/FuzzyText/FuzzyText";
-
-interface Props {
-    index: number,
-}
 
 function ProjectSection() {
 
     return (
-      <section  >
-        <FuzzyText 
-          baseIntensity={0.2} 
-        >
-          404
-        </FuzzyText>
+      <section>
+        
       </section>
     )
 }
